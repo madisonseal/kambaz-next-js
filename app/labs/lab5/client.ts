4baef89b29b07882f0d3478da7b73419a6130f42
[madisonseal1] <madisonseal@icloud.com>
2026-03-29 17:28:30 -0400

    fix lab5 client HTTP_SERVER

--- app/labs/lab5/client.ts
+++ app/labs/lab5/client.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-// const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER || "http://localhost:4000";
-const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
+const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER || "http://localhost:4000";
 export const fetchWelcomeMessage = async () => {
   const response = await axios.get(`${HTTP_SERVER}/lab5/welcome`);
   return response.data;
